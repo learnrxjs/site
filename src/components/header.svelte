237d0fec0e549a901de7988ae699328903616145
Sam Bulatov <mephistorine@gmail.com>
2022-09-03 20:55:01 +0300

    feat: Начата работа над структурой проекта

---
+++ src/components/header.svelte
@@ -0,0 +1,34 @@
+<script>
+import Icon from "./icon.svelte";
+</script>
+
+<header class="border-b py-4 px-8">
+  <div class="wrap">
+    <div class="flex justify-between items-center">
+      <a href="/">
+        <span class="text-xl font-bold">RxJS Space</span>
+      </a>
+    
+      <nav class="hidden md:block">
+        <ul class="flex items-center gap-8">
+          <li>
+            <a class=" text-gray-500 hover:text-pink-500" href="/overview">Введение</a>
+          </li>
+          <li>
+            <a class=" text-gray-500 hover:text-pink-500" href="/api">API</a>
+          </li>
+          <li>
+            <a class=" text-gray-500 hover:text-pink-500" href="/blog">Блог</a>
+          </li>
+          <li>
+            <a class=" text-gray-500 hover:text-pink-500" href="/tour">Туториал</a>
+          </li>
+        </ul>
+      </nav>
+  
+      <button class="w-[2em] h-[2em] md:hidden">
+        <Icon name="menu" />
+      </button>
+    </div>
+  </div>
+</header>
---
+++ src/components/icon.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  type IconNames = "menu"
+  const icons: Record<IconNames, string> = {
+    "menu": `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+      <path fill="none" d="M0 0h256v256H0z"/>
+      <path stroke="var(--icon-color, currentColor)" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" d="M40 128h176M40 64h176M40 192h176"/>
+    </svg>`
+  }
+
+  export let name: IconNames;
+  $: iconHtml = Reflect.get(icons, name)
+</script>
+
+{@html iconHtml}
